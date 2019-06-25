e2427d85b3170cad41ff3ac4bd5c2631c454362d
HerobrainGamer <herobraingaming@gmail.com>
2019-06-24 20:57:56 -0700

    code
    Fixed issues with sword and axe base files not being public and spelled correctly

--- 1.14/Normal 1.14.2/src/main/java/pixilaregames/pixelcore/API/ItemsBase/ItemAxeBase.java
+++ 1.14/Normal 1.14.2/src/main/java/pixilaregames/pixelcore/API/ItemsBase/ItemAxeBase.java
@@ -6,7 +6,7 @@ import net.minecraft.item.IItemTier;
 public class ItemAxeBase extends AxeItem
 {
 
-	protected ItemAxeBase(IItemTier tier, float p_i48530_2_, float attackSpeedIn, Properties builder)
+	public ItemAxeBase(IItemTier tier, float p_i48530_2_, float attackSpeedIn, Properties builder)
 	{
 		super(tier, p_i48530_2_, attackSpeedIn, builder);
 	}
--- 1.14/Normal 1.14.2/src/main/java/pixilaregames/pixelcore/API/ItemsBase/ItemSwordBase.java
+++ 1.14/Normal 1.14.2/src/main/java/pixilaregames/pixelcore/API/ItemsBase/ItemSwordBase.java
@@ -3,10 +3,10 @@ package pixilaregames.pixelcore.API.ItemsBase;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.SwordItem;
 
-public class ItemSowrdBase extends SwordItem
+public class ItemSwordBase extends SwordItem
 {
 
-	public ItemSowrdBase(IItemTier teir, int attackDamageIn, float attackSpeedIn, Properties builder)
+	public ItemSwordBase(IItemTier teir, int attackDamageIn, float attackSpeedIn, Properties builder)
 	{
 		super(teir, attackDamageIn, attackSpeedIn, builder);
 	}
