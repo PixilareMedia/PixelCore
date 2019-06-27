7d7c796bcf14324cfeb3fee04a3d23e926fba743
HerobrainGamer <herobraingaming@gmail.com>
2019-06-27 14:45:57 -0700

    code
    - Figured out how to add items from vanilla to a custom tab

--- 1.14/Normal 1.14.2/src/main/java/pixilaregames/pixelcore/ItemGroups/CommandExclusivesItemGroup.java
+++ 1.14/Normal 1.14.2/src/main/java/pixilaregames/pixelcore/ItemGroups/CommandExclusivesItemGroup.java
@@ -3,6 +3,7 @@ package pixilaregames.pixelcore.ItemGroups;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.NonNullList;
 
 public class CommandExclusivesItemGroup extends ItemGroup
@@ -22,5 +23,19 @@ public class CommandExclusivesItemGroup extends ItemGroup
 	public void fill(NonNullList<ItemStack> items)
 	{
 		super.fill(items);
+		
+		items.add(new ItemStack(Items.COMMAND_BLOCK));
+		items.add(new ItemStack(Items.CHAIN_COMMAND_BLOCK));
+		items.add(new ItemStack(Items.REPEATING_COMMAND_BLOCK));
+		items.add(new ItemStack(Items.COMMAND_BLOCK_MINECART));
+		items.add(new ItemStack(Items.BARRIER));
+		items.add(new ItemStack(Items.STRUCTURE_BLOCK));
+		items.add(new ItemStack(Items.STRUCTURE_VOID));
+		items.add(new ItemStack(Items.JIGSAW));
+		items.add(new ItemStack(Items.DEBUG_STICK));
+		items.add(new ItemStack(Items.SPAWNER));
+		items.add(new ItemStack(Items.DRAGON_EGG));
+		items.add(new ItemStack(Items.KNOWLEDGE_BOOK));
+		items.add(new ItemStack(Blocks.NETHER_PORTAL));
 	}
 }
